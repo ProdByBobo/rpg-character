9852423147e9e6bd24d718f675cb51e35eae9df1
Shane Godfrey <scg5440@psu.edu>
2025-03-18 17:06:40 -0400

    Update rpg-character.js

--- rpg-character.js
+++ rpg-character.js
@@ -1,85 +1,22 @@
-
-import { LitElement, html, css } from "lit";
-import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
-import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
-
-/**
- * `rpg-character`
- * 
- * @demo index.html
- * @element rpg-character
- */
-export class RpgCharacter extends DDDSuper(I18NMixin(LitElement)) {
-
-  static get tag() {
-    return "rpg-character";
-  }
-
+class RPGCharacter extends HTMLElement {
   constructor() {
     super();
-<<<<<<< HEAD
-    this.title = "";
-=======
-    this._data = "https://www.psu.edu/";
     this.title = "RPG Characters";
-    this.header = ""
->>>>>>> 72af58fbe58411c0ce4fac5d8538f6e2c0d84ae9
-    this.t = this.t || {};
+    this._data = "https://www.psu.edu/";
+    this.header = "";
     this.t = {
-      ...this.t,
       title: "Title",
+      ...this.t,
     };
     this.registerLocalization({
       context: this,
-      localesPath:
-        new URL("./locales/rpg-character.ar.json", import.meta.url).href +
-        "/../",
+      localesPath: new URL("./locales/rpg-character.ar.json", import.meta.url).href + "/../",
       locales: ["ar", "es", "hi", "zh"],
     });
   }
 
-  // Lit reactive properties
-  static get properties() {
-    return {
-      ...super.properties,
-      title: { type: String },
-    };
-  }
-
-  // Lit scoped styles
-  static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      h3 span {
-        font-size: var(--rpg-character-label-font-size, var(--ddd-font-size-s));
-      }
-    `];
-  }
-
-  // Lit render the HTML
-  render() {
-    return html`
-<div class="wrapper">
-  <h3><span>${this.t.title}:</span> ${this.title}</h3>
-  <slot></slot>
-</div>`;
-  }
-
- 
-  static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+  registerLocalization({ context, localesPath, locales }) {
   }
 }
 
-globalThis.customElements.define(RpgCharacter.tag, RpgCharacter);
+customElements.define('rpg-character', RPGCharacter);
